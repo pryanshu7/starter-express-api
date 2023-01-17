fa6ad0b883ccd03b46f64366ef2daa76ae52f58d
pryanshu7 <priyanshu_singh@srmap.edu.in>
2023-01-17 12:38:39 +0530

    17th Jan 2023

--- backend/constants/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGO_URL: process.env.MONGO_URL,
-  OPEN_EVENT: "open",
-  ERROR_EVENT: "error",
-  DISCONNECTED_EVENT: "disconnected",
-  RECONNECT_FAILED_EVENT: "reconnectFailed",
-};
